92af42a092002732baadd30b48e1b4f6b08697ea
smit1678 <nate@developmentseed.org>
2016-03-03 16:54:34 +0000

    adds post to s3 functionality, removes flask

--- taskingDbEndpoint.py
+++ taskingDbEndpoint.py
@@ -1,13 +1,32 @@
+#!/usr/bin/python
+
+# This script creates statistics for OSM users of the OSM Tasking Manager database. 
+# Edit timestamps for each category of action arranged by project and user. 
+# Output is in JSON format and is uploaded directly to an Amazon S3 bucket.
+#
+# Environment variables:
+#
+#   S3_ACCESS_KEY=your_access_key
+#   S3_SECRET_KEY=your_secret_key
+#   BUCKET=your_bucket
+#
+# Usage
+#
+# python taskingDBendpoint.py
+
 import psycopg2
 import psycopg2.extras
-from flask import Flask, jsonify
-
+import tinys3
+import simplejson as json
+from datetime import datetime
+import time
+import os
 
 def connectDB():
     # login info
     host = 'localhost'
-    database = 'osmtm'
-    user = 'postgres'
+    database = 'tasking'
+    user = 'nsmith'
     # define connection string
     conn_string = "host='%s' dbname='%s' user='%s'" % (host, database, user)
     # get a connection
@@ -30,33 +49,42 @@ def getTaskstate():
     # project_id, and placeholders for edit type and times
     stateLookup = {2: 'done', 3: 'validated', 1: 'invalidated'}
     users = {}
+    # loop through all records from database query to generate per user data
     for r in records:
         user_id = str(r.user_id)
         proj_id = str(r.project_id)
+        # create empty dict for each user
+        users[user_id] = {}
+        # check if user exists in the users dict
         if user_id in users:
             if proj_id in users[user_id]:
                 users[user_id][proj_id][stateLookup[r.state]]['times']\
-                    .append(r.date)
+                    .append(str(r.date).split(".")[0])
             else:
                 users[user_id][proj_id] = {'done': {'times': []},
                                            'validated': {'times': []},
                                            'invalidated': {'times': []}}
-        else:
-            users[user_id] = {}
 
     return users
 
+def upload(file):
+    # uses tinys3 to create connection, open file, and upload
+    # get access keys and buckets from environment variables
+    conn = tinys3.Connection(os.getenv('S3_ACCESS_KEY'), os.getenv('S3_SECRET_KEY'), tls=True)
+    f = open('%s.json' % file, 'rb')
+    conn.upload('%s.json' % file, f, bucket=os.getenv('BUCKET'), content_type='application/json')
 
-app = Flask(__name__)
-
-
-# route definitions
-# posts dictionary from getUserdata function as JSON
-@app.route('/')
-def userdata():
+def main():
     users = getTaskstate()
-    return jsonify(users)
+
+    # dump users dict into minified json
+    fout = json.dumps(users, separators=(',',':'))
+    # generate file of users json
+    f = open('users.json', 'wb')
+    f.write(fout)
+    f.close()
+    # trigger upload to s3
+    upload('users')
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    main()
